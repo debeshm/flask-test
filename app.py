6709f7da9a8b4355ed664e6cf35c3ef8fbaadfc1
Debesh Mohanty <debeshmohanty@Debeshs-MacBook-Pro.local>
2024-07-13 15:02:48 -0700

    second commit

--- app.py
+++ app.py
@@ -1,14 +1,11 @@
 from flask import Flask, render_template, request, redirect, url_for
 import datetime
 
-def create_app():
-    app = Flask(__name__)
+app = Flask(__name__)
 
-    @app.route("/", methods=["GET", "POST"])
-    def home():
-        return "Hello world test"
+@app.route("/", methods=["GET", "POST"])
+def home():
+    return "Hello world test"
 
-    if __name__ == '__main__':
-        app.run(debug=False , host='0.0.0.0' )
-
-    return app
+if __name__ == '__main__':
+    app.run(debug=False , host='0.0.0.0' )
